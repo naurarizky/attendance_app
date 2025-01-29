774f346c13ecd6c6bbbf59e4b68a5fd42e3f88c2
naurarizzky <naura.rizky404@smk.belajar.id>
2025-01-29 10:15:27 +0700

    make a header component

---
+++ lib/ui/attend/components/header.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+Container buildHeader() {
+  return Container(
+    height: 50,
+    decoration: const BoxDecoration(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(10),
+        topRight: Radius.circular(10),
+      ),
+      color: Colors.blueAccent,
+    ),
+    child: const Row(
+      children: [
+        SizedBox(width: 12),
+        Icon(Icons.face_retouching_natural_outlined,
+        color: Colors.white
+        ), 
+        SizedBox(width: 12),
+        Text(
+          "Please take a picture",
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+            fontWeight: FontWeight.bold
+          ),
+        )
+      ],
+    ),
+    );
+}
