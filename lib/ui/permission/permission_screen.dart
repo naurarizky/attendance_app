05cb889650f3589d486a1f4d1b873c2658309398
naurarizzky <naura.rizky404@smk.belajar.id>
2025-02-06 13:32:00 +0700

    creating from header placeholder

--- lib/ui/permission/permission_screen.dart
+++ lib/ui/permission/permission_screen.dart
@@ -1,4 +1,5 @@
 import 'package:attendace_app/services/location_services.dart';
+import 'package:attendace_app/ui/attend/components/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -12,14 +13,26 @@ class PermissionScreen extends StatefulWidget {
 class _PermissionScreenState extends State<PermissionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-
-  void requestPermission() async{
-    final hasPermission = await handleLocationPermission(context);
-
-    // try {
-    //   if
-    // }
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: buildAppBar(context),
+      body: SingleChildScrollView(
+        child: Card(
+          color: Colors.white,
+          margin: const EdgeInsets.fromLTRB(10, 10, 10, 30),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+          elevation: 5,
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
